700f2df196152d0883e4b81e2444f8a4b11de585
lmsgtv <linar_97@list.ru>
2025-05-30 16:09:23 +0500

    token endpoint + fixes

--- app/auth/router.py
+++ app/auth/router.py
@@ -1,19 +1,20 @@
-from typing import List
+from typing import List, Annotated
 from fastapi import APIRouter, Response, Depends
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.auth.models import User
-from app.auth.utils import authenticate_user, set_tokens
+from app.auth.utils import authenticate_user, set_tokens, create_tokens
 from app.dependencies.auth_dep import get_current_user, get_current_admin_user, check_refresh_token
 from app.dependencies.dao_dep import get_session_with_commit, get_session_without_commit
 from app.exceptions import UserAlreadyExistsException, IncorrectEmailOrPasswordException
 from app.auth.dao import UsersDAO
-from app.auth.schemas import SUserRegister, SUserAuth, UsernameModel, SUserAddDB, SUserInfo
+from app.auth.schemas import SUserRegister, SUserAuth, UsernameModel, SUserAddDB, SUserInfo, STokens, SRefreshToken
 
 router = APIRouter()
 
 
-@router.post("/register/")
+@router.post("/register")
 async def register_user(user_data: SUserRegister,
                         session: AsyncSession = Depends(get_session_with_commit)) -> dict:
     # Проверка существования пользователя
@@ -36,7 +37,7 @@ async def register_user(user_data: SUserRegister,
     return {'message': 'Вы успешно зарегистрированы!'}
 
 
-@router.post("/login/")
+@router.post("/login")
 async def auth_user(
         response: Response,
         user_data: SUserAuth,
@@ -56,6 +57,24 @@ async def auth_user(
     }
 
 
+@router.post("/token")
+async def get_tokens(
+        form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+        session: AsyncSession = Depends(get_session_without_commit),
+) -> STokens:
+    users_dao = UsersDAO(session)
+    user = await users_dao.find_one_or_none(
+        filters=UsernameModel(username=form_data.username)
+    )
+
+    if not (user and await authenticate_user(user=user, password=form_data.password)):
+        raise IncorrectEmailOrPasswordException(
+            headers={'WWW-Authenticate': 'Bearer'},
+        )
+
+    return STokens(**create_tokens(data={"sub": str(user.id)}))
+
+
 @router.post("/logout")
 async def logout(response: Response):
     response.delete_cookie("user_access_token")
@@ -63,13 +82,13 @@ async def logout(response: Response):
     return {'message': 'Пользователь успешно вышел из системы'}
 
 
-@router.get("/me/")
+@router.get("/me")
 async def get_me(user_data: User = Depends(get_current_user)) -> SUserInfo:
     return SUserInfo.model_validate(user_data)
 
 
 @router.get(
-    "/all_users/",
+    "/all_users",
     dependencies=[Depends(get_current_admin_user)],
 )
 async def get_all_users(
@@ -80,8 +99,9 @@ async def get_all_users(
 
 @router.post("/refresh")
 async def process_refresh_token(
-        response: Response,
-        user: User = Depends(check_refresh_token)
-):
-    set_tokens(response, user.id)
-    return {"message": "Токены успешно обновлены"}
+        session: Annotated[AsyncSession, Depends(get_session_without_commit)],
+        refresh_token: SRefreshToken,
+) -> STokens:
+    user = await get_current_user(token=refresh_token.refresh_token, session=session)
+
+    return STokens(**create_tokens(data={"sub": str(user.id)}))
