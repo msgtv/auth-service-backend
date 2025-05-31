4d6596d38cf0462f0610b5954e3358f586ecedba
lmsgtv <linar_97@list.ru>
2025-05-31 10:41:19 +0500

    token_service & password_service oop

--- app/auth/utils.py
+++ app/auth/utils.py
@@ -1,104 +1,202 @@
-from typing import Optional
+from typing import Literal
 from datetime import datetime, timedelta, timezone
 
 from passlib.context import CryptContext
 from jose import jwt
-from fastapi.responses import Response
 
 from app.config import settings
-from app.auth.dao import UsersDAO
-from app.auth.redis_manager import redis_manager
+from app.auth.redis_manager import RedisTokenManager
+from app.auth.models import User
 
 
-def create_tokens(data: dict, session_id: str) -> dict:
-    """Создать новую пару токенов
+class TokenService:
+    """Сервис для работы с токенами"""
+
+    def __init__(self):
+        self.redis_manager = RedisTokenManager()
     
-    Args:
-        data: Данные для включения в токен
-        session_id: ID сессии
-    """
-    # Текущее время в UTC
-    now = datetime.now(timezone.utc)
-
-    # AccessToken - 30 минут
-    access_expire = now + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_payload = data.copy()
-    access_payload.update({"exp": int(access_expire.timestamp()), "type": "access"})
-    access_token = jwt.encode(
-        access_payload,
-        settings.SECRET_KEY,
-        algorithm=settings.ALGORITHM
-    )
-
-    # RefreshToken - 7 дней
-    refresh_expire = now + timedelta(days=settings.REFRESH_TOKEN_EXPIRE_DAYS)
-    refresh_payload = data.copy()
-    refresh_payload.update({"exp": int(refresh_expire.timestamp()), "type": "refresh"})
-    refresh_token = jwt.encode(
-        refresh_payload,
-        settings.SECRET_KEY,
-        algorithm=settings.ALGORITHM
-    )
-
-    # Store tokens in Redis
-    redis_manager.store_token(
-        subject=data["sub"],
-        token=access_token,
-        token_type="access",
-        expire_time=settings.ACCESS_TOKEN_EXPIRE_MINUTES * 60,
-        session_id=session_id
-    )
+    def _create_token(
+            self,
+            payload: dict,
+            token_type: Literal['access', 'refresh'],
+            expire_time: datetime,
+    ) -> str:
+        """Создать токен"""
+        
+        payload.update({"exp": int(expire_time.timestamp()), "type": token_type})
+        return jwt.encode(
+            payload,
+            settings.SECRET_KEY,
+            algorithm=settings.ALGORITHM
+        )
     
-    redis_manager.store_token(
-        subject=data["sub"],
-        token=refresh_token,
-        token_type="refresh",
-        expire_time=settings.REFRESH_TOKEN_EXPIRE_DAYS * 24 * 60 * 60,
-        session_id=session_id
-    )
-
-    return {
-        "access_token": access_token,
-        "refresh_token": refresh_token,
-    }
-
-
-async def authenticate_user(user, password):
-    if (
-            not user
-            or
-            verify_password(plain_password=password, hashed_password=user.password) is False
+    def _store_token(
+            self,
+            subject: int | str,
+            token: str,
+            token_type: Literal['access', 'refresh'],
+            expire_time: datetime,
+            client_fingerprint: str,
     ):
-        return None
-    return user
+        self.redis_manager.store_token(
+            subject=subject,
+            token=token,
+            token_type=token_type,
+            expire_time=expire_time,
+            client_fingerprint=client_fingerprint,
+        )
+    
+    def create_tokens(
+            self,
+            data: dict,
+            client_fingerprint: str,
+    ) -> dict[str, str]:
+        """
+        Создать новую пару токенов
+        
+        Args:
+            data: Данные для включения в токен
+            client_fingerprint: ID сессии
+        """
+        # Текущее время в UTC
+        now = datetime.now(timezone.utc)
+
+        # AccessToken 
+        access_token = self._create_token(
+            payload=data.copy(),
+            token_type="access",
+            expire_time=now + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+        )
+        
+        # RefreshToken
+        refresh_token = self._create_token(
+            payload=data.copy(),
+            token_type="refresh",
+            expire_time=now + timedelta(days=settings.REFRESH_TOKEN_EXPIRE_DAYS)
+        )
+
+        # Store tokens in Redis
+        self._store_token(
+            subject=data["sub"],
+            token=access_token,
+            token_type="access",
+            expire_time=settings.ACCESS_TOKEN_EXPIRE_MINUTES * 60,
+            client_fingerprint=client_fingerprint
+        )
+        
+        self._store_token(
+            subject=data["sub"],
+            token=refresh_token,
+            token_type="refresh",
+            expire_time=settings.REFRESH_TOKEN_EXPIRE_DAYS * 24 * 60 * 60,
+            client_fingerprint=client_fingerprint
+        )
+
+        return {
+            "access_token": access_token,
+            "refresh_token": refresh_token,
+        }
+
+    def verify_token(
+            self,
+            token: str,
+            user_id: int,
+            token_type: Literal["access", "refresh"],
+            client_fingerprint: str,
+    ) -> bool:
+        """
+        Проверить валидность токена
+
+        Args:
+            token: Токен
+            user_id: ID пользователя
+            token_type: Тип токена (access или refresh)
+            client_fingerprint: ID сессии
+        """
+        return self.redis_manager.is_token_verified(
+            subject=user_id,
+            token_type=token_type,
+            token=token,
+            client_fingerprint=client_fingerprint,
+        )
+
+    def invalidate_token(
+            self,
+            user_id: int,
+            token_type: Literal["access", "refresh"],
+            client_fingerprint: str,
+    ) -> None:
+        """
+        Инвалидировать токен
+        
+        Args:
+            user_id: ID пользователя
+            token_type: Тип токена (access или refresh)
+            client_fingerprint: ID сессии
+        """
+        self.redis_manager.invalidate_token(
+            subject=user_id,
+            token_type=token_type,
+            client_fingerprint=client_fingerprint,
+        )
+
+    def invalidate_token_pair(
+            self,
+            user_id: int,
+            client_fingerprint: str,
+    ) -> None:
+        """
+        Инвалидировать пару токенов
+        
+        Args:
+            user_id: ID пользователя
+            client_fingerprint: ID сессии
+        """
+        self.redis_manager.invalidate_token_pair(
+            subject=user_id,
+            client_fingerprint=client_fingerprint,
+        )
+
+    def invalidate_all_tokens(
+            self,
+            user_id: int,
+    ) -> None:
+        """
+        Инвалидировать все токены пользователя на всех устройствах
+        
+        Args:
+            user_id: ID пользователя
+        """
+        self.redis_manager.invalidate_all_user_tokens(user_id)
+
+    def decode_token(self, token: str) -> dict:
+        """Декодировать токен"""
+        return jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+
+class PasswordService:
+    """Сервис для работы с паролями"""
+
+    def __init__(self):
+        self.pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+    def get_password_hash(self, password: str) -> str:
+        """Получить хеш пароля"""
+        return self.pwd_context.hash(password)
+
+    def verify_password(self, plain_password: str, hashed_password: str) -> bool:
+        """Проверить пароль"""
+        return self.pwd_context.verify(plain_password, hashed_password)
+
+    def authenticate_user(self, user: User, password: str) -> User | None:
+        """Аутентифицировать пользователя"""
+        if (
+                not user
+                or
+                self.verify_password(plain_password=password, hashed_password=user.password) is False
+        ):
+            return None
+        return user
 
 
-def invalidate_session_tokens(user_id: int, session_id: str):
-    """Инвалидировать все токены для определенного устройства"""
-    redis_manager.invalidate_session_tokens(user_id, session_id)
-
-
-def invalidate_all_user_tokens(user_id: int):
-    """Инвалидировать все токены пользователя на всех устройствах"""
-    redis_manager.invalidate_all_user_tokens(user_id)
-
-
-def get_user_sessions(user_id: int) -> list:
-    """Получить список активных сессий пользователя"""
-    return redis_manager.get_user_sessions(user_id)
-
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-
-def get_password_hash(password: str) -> str:
-    return pwd_context.hash(password)
-
-
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
-
-
-def verify_token(token: str, user_id: int, token_type: str, session_id: str) -> bool:
-    """Проверить валидность токена"""
-    return redis_manager.is_token_verified(user_id, token_type, token, session_id)
+token_service = TokenService()
+password_service = PasswordService()
